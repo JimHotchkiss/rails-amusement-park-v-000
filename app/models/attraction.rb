12e54b4f105896cae7e478dbc1a859adf5c2d6e2
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-09-30 07:30:50 -0400

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,5 +1,4 @@
 class Attraction < ActiveRecord::Base
-  # write associations here
   has_many :rides
-  #has_many :users, through: :rides
+  has_many :users, through: :rides 
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,24 @@
 class Ride < ActiveRecord::Base
-  # write associations here
-  belongs_to :attraction
   belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    @user = User.find_by(id: self.user_id)
+    @attraction = Attraction.find_by(id: self.attraction_id)
+
+    if @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif @user.tickets < @attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif @user.height < @attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      @user.tickets = @user.tickets - @attraction.tickets
+      @user.nausea = @user.nausea + @attraction.nausea_rating
+      @user.happiness = @user.happiness + @attraction.happiness_rating
+      @user.save
+    end
+  end
+
+
 end
